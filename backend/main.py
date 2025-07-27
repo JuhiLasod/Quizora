c87e5a0d0aab8aab77b2f5d0f140bf3ac2f3745e
JuhiLasod <juhilasod29@gmail.com>
2025-07-27 19:03:11 +0530

    frontend backend set db connection set

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from model import Users
+import os
+load_dotenv()
 
 app=FastAPI()
+
+client=MongoClient(os.getenv("MONGO_URI"))
+db=client["Quizora"]
+collection=db["Users"]
+
 @app.get("/")
-def first():
-    print("hellooooooo")
-    return "hello juhi"
+def root():
+    return {"message": "backend running"}
+
+@app.post("/users")
+def users(user: Users):
+    collection.insert_one(user.dict())
+    return {"message": "user added"}
